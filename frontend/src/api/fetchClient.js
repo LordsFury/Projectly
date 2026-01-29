e9433c426d17a763fad7080dedcbdfb7e1af2290
LordFury <iabdullahkhan100@gmail.com>
2026-01-29 15:00:08 +0500

    api fixed

--- frontend/src/api/fetchClient.js
+++ frontend/src/api/fetchClient.js
@@ -1,8 +1,6 @@
-const API_URL = "VITE_API_URL/api";
-
 export const fetchClient = async (url, options = {}) => {
   const token = localStorage.getItem("token");
-  const res = await fetch(API_URL + url, {
+  const res = await fetch(process.env.VITE_API_URL + url, {
     headers: {
       "Content-Type": "application/json",
       ...(token && { Authorization: `Bearer ${token}` }),
